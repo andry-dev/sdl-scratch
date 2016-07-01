ed58cb8f6a46e57d5539329d31e55e34ee40c548
andry-dev <peketribal2@gmail.com>
2016-07-01 17:50:34 +0200

    Trying my best to support Windows

--- main_program/src/main.cpp
+++ main_program/src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+
+#include <SDL2/SDL.h>
+
+#define WINLEL_WIDTH 640
+#define WINLEL_HEIGHT 480
 
 int main()
 {
-	std::cout << "We \n";
+	SDL_Window* window;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	window = SDL_CreateWindow(
+			"Test",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			WINLEL_WIDTH,
+			WINLEL_HEIGHT,
+			SDL_WINDOW_OPENGL);
+
+	while (true) {
+
+	}
 
 	return 0;
 }
+
+
